8258ef7ddceae8b330381bd4f727316410b0291b
romeroplata <jalpa1976>
2014-12-04 10:54:01 -0600

    Signed-off-by: romeroplata <jalpa1976>

--- project/aprende_a_sumar_web/getForm.js
+++ project/aprende_a_sumar_web/getForm.js
@@ -4,7 +4,7 @@ function getForm(url){
 	var info=url.split("?");
 	var nameValuePairs=info[1].split("&");
 	var $_GET = new Object;
-	for(var i=0;i<nameValuePairs.length-1;i++){
+	for(var i=0;i<nameValuePairs.length;i++){
 		var map=nameValuePairs[i].split("=");
 		var name=map[0];
 		var value=map[1];
